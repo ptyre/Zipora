52ac13452a9e9c724ca4efbad39c65d4a606302e
naufaldwimln <41778770+naufaldwimln@users.noreply.github.com>
2019-01-28 14:33:47 +0700

    awal
    1

---
+++ api/getInfoBuku.php
@@ -0,0 +1,22 @@
+<?php
+	include 'connection.php';
+
+	$con = mysqli_connect($host, $user, $password, $db);
+
+	if (!$con) {
+		die(mysqli_connect_error($con));
+	}
+
+	$qr = "SELECT * FROM tb_buku";
+	$result = mysqli_query($con, $qr);
+
+	if (mysqli_num_rows($result) > 0) {
+		$arr_rak = array();
+		while ($row = mysqli_fetch_assoc($result)) {
+			$arr_rak[] = $row;
+		}
+		echo json_encode($arr_rak);
+	}
+	else {
+		echo "Tidak terdapat";
+	}
