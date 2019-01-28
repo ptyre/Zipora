080a0c258a77c42efd1debdb3e87bb51f1eda4f0
ptyre <ptyreasawau1@Gmail.com>
2019-01-28 08:12:36 +0700

    Repair
    hello

---
+++ ziporatwo/resources/views/admin/partial/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/bootstrap.min.css') }}css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('fonts/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
+    @yield('css')
+    <title></title>
+  </head>
+  <body>
+    @include('admin.partial.navbar')
+    @yield('content')
+    @yield('script')
+  </body>
+</html>
