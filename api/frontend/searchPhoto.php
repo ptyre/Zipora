10b925220f7b37856e93d0e5e0d7cac19a1c7000
naufaldwimln <41778770+naufaldwimln@users.noreply.github.com>
2019-02-13 11:19:40 +0700

    Create searchPhoto.php

---
+++ api/frontend/searchPhoto.php
@@ -0,0 +1,24 @@
+<?php
+	include 'connection.php';
+
+	$con = mysqli_connect($host, $user, $password, $db);
+
+	if (!$con) {
+		die(mysqli_connect_error($con));
+	}
+
+	$jenis = $_POST['jenis'];
+
+	$query = "SELECT tb_galery.id, tb_galery.pict, tb_galery.info, tb_galery.tgl, tb_galery.jenis FROM tb_galery WHERE jenis = '$jenis'";
+	$result = mysqli_query($con, $query);
+
+	if (mysqli_num_rows($result) > 0) {
+		$arr_rak = array();
+		while ($row = mysqli_fetch_assoc($result)) {
+			$arr_rak[] = $row;
+		}
+		echo json_encode($arr_rak);
+	}
+	else {
+		echo "Tidak terdapat";
+	}
