94ea3d1e5c88549aa5b5be5d05655e370c187026
naufaldwimln <41778770+naufaldwimln@users.noreply.github.com>
2019-02-15 08:41:02 +0700

    Update dashboard.php

--- api/frontend/dashboard.php
+++ api/frontend/dashboard.php
@@ -7,7 +7,7 @@
 		die(mysqli_connect_error($con));
 	}
 
-	$query = mysqli_query($con, "SELECT tb_informasi.id, tb_informasi.judul, tb_informasi.pict, tb_informasi.informasi, tb_informasi.tgl, tb_jenisinfo.jenisinformasi FROM tb_informasi INNER JOIN tb_jenisinfo ON tb_jenisinfo.id = tb_informasi.id_jenisinfo WHERE jenisinformasi != 'article'");
+	$query = mysqli_query($con, "SELECT tb_informasi.id, tb_informasi.judul, tb_informasi.pict, tb_informasi.informasi, tb_informasi.tgl, tb_jenisinfo.jenisinformasi FROM tb_informasi INNER JOIN tb_jenisinfo ON tb_jenisinfo.id = tb_informasi.id_jenisinfo WHERE jenisinformasi != 'article' ORDER BY tb_informasi.id DESC");
 
 	$result = array();
 	while ($row = mysqli_fetch_array($query)) {
@@ -21,7 +21,7 @@
 		));
 	}
 
-	$query = mysqli_query($con, "SELECT tb_informasi.id, tb_informasi.judul, tb_informasi.pict, tb_informasi.informasi, tb_informasi.tgl, tb_jenisinfo.jenisinformasi FROM tb_informasi INNER JOIN tb_jenisinfo ON tb_jenisinfo.id = tb_informasi.id_jenisinfo WHERE jenisinformasi = 'article'");
+	$query = mysqli_query($con, "SELECT tb_informasi.id, tb_informasi.judul, tb_informasi.pict, tb_informasi.informasi, tb_informasi.tgl, tb_jenisinfo.jenisinformasi FROM tb_informasi INNER JOIN tb_jenisinfo ON tb_jenisinfo.id = tb_informasi.id_jenisinfo WHERE jenisinformasi = 'article' ORDER BY tb_informasi.id DESC");
 
 	$result2 = array();
 	while ($row = mysqli_fetch_array($query)) {
